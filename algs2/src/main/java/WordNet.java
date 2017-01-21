fabbe9ee497a427d5312ed17cff33fb4719e8147
vreuter <vince.reuter@gmail.com>
2017-01-20 17:44:04 -0700

    WordNet stubs

---
+++ algs2/src/main/java/WordNet.java
@@ -0,0 +1,45 @@
+import edu.princeton.cs.algs4.Digraph;
+
+
+/**
+ * English-language semantic lexicon representing
+ * relationships between words. A relationship may
+ * indicate synonyms (synset of words), or hypernyms
+ * (a set of synonyms that encompasses another). This
+ * ADT uses a directed graph to represent WordNet.
+ */
+public class WordNet {
+
+
+    public WordNet(String synsets, String hypernyms) {
+
+    }
+
+
+    public Iterable<String> nouns() {
+
+    }
+
+
+    public boolean isNoun(String word) {
+
+    }
+
+
+    public int distance(String nounA, String nounB) {
+
+    }
+
+
+    public String sap(String nounA, String nounB) {
+
+    }
+
+
+    /* Crude testing */
+    public static void main(String[] args) {
+
+    }
+
+
+}
